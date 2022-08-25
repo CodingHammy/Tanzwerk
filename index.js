cabd4646b61d09996ed6b2ccdaaacb06762d3a34
CodingHammy <Will.hamilton.job@gmail.com>
2022-08-25 17:05:56 +0100

    pre julius help

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ let headers = document.querySelector('#Das');
 let navBar = document.querySelector('#navBarParent');
 let navBarjus = document.querySelector('#navBar');
 let navPos = navBar.getBoundingClientRect().top;
+let navWil = document.querySelector('#navWil');
+navWil.classList.add('inactive');
 
 window.addEventListener('scroll', (e) => {
   scrollPos = window.scrollY;
   if (scrollPos > navPos) {
     navBar.classList.add('sticky');
-    headers.classList.add('navbarOffsetMargin');
   } else {
     navBar.classList.remove('sticky');
-    headers.classList.remove('navbarOffsetMargin');
   }
 
   navBarLinks.forEach((link) => {
     let section = document.querySelector(link.hash);
-    // console.log(link.hash);
     if (
-      scrollPos + 100 > section.offsetTop &&
-      scrollPos + 100 < section.offsetTop + section.offsetHeight
+      scrollPos > section.offsetTop &&
+      scrollPos < section.offsetTop + section.clientHeight
     ) {
       link.parentElement.classList.add('inactive');
     } else {
       link.parentElement.classList.remove('inactive');
     }
   });
+  if (scrollPos === 0 || navBarLinks === '#navBar') {
+    navWil.classList.add('inactive');
+  }
 });
