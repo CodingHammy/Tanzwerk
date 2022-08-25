64f1720ed45351bdbb6e0f89914487bb152ff594
CodingHammy <Will.hamilton.job@gmail.com>
2022-08-25 16:08:35 +0100

    navBar javascript implimented

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-let navWil = document.querySelector('#navWil');
-let navDas = document.querySelector('#navDas');
-let navWas = document.querySelector('#navWas');
-let navKon = document.querySelector('#navKon');
+let navBarLinks = document.querySelectorAll('nav a');
 let headers = document.querySelector('#Das');
 
 let navBar = document.querySelector('#navBarParent');
+let navBarjus = document.querySelector('#navBar');
 let navPos = navBar.getBoundingClientRect().top;
-navWil.classList.add('inactive');
 
 window.addEventListener('scroll', (e) => {
   scrollPos = window.scrollY;
-  console.log(scrollPos);
   if (scrollPos > navPos) {
     navBar.classList.add('sticky');
     headers.classList.add('navbarOffsetMargin');
-    navWil.classList.remove('inactive');
   } else {
-    navWil.classList.add('inactive');
     navBar.classList.remove('sticky');
     headers.classList.remove('navbarOffsetMargin');
   }
+
+  navBarLinks.forEach((link) => {
+    let section = document.querySelector(link.hash);
+    // console.log(link.hash);
+    if (
+      scrollPos + 100 > section.offsetTop &&
+      scrollPos + 100 < section.offsetTop + section.offsetHeight
+    ) {
+      link.parentElement.classList.add('inactive');
+    } else {
+      link.parentElement.classList.remove('inactive');
+    }
+  });
 });
