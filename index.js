365810c34db9e9704c5425438902d41c4aa30e41
CodingHammy <Will.hamilton.job@gmail.com>
2022-08-26 16:19:43 +0100

    navbar hamburger and visablity vixed

--- index.js
+++ index.js
@@ -2,31 +2,42 @@ let navBarLinks = document.querySelectorAll('nav a');
 let headers = document.querySelector('#Das');
 
 let navBar = document.querySelector('#navBarParent');
-let navBarjus = document.querySelector('#navBar');
 let navPos = navBar.getBoundingClientRect().top;
-let navWil = document.querySelector('#navWil');
-navWil.classList.add('inactive');
+
+function onClick() {
+  document.querySelector('#menu').classList.toggle('hamburger');
+  document.querySelector('#navBar').classList.toggle('hamburger');
+}
+document.querySelector('#menu').classList.add('visable');
 
 window.addEventListener('scroll', (e) => {
   scrollPos = window.scrollY;
   if (scrollPos > navPos) {
     navBar.classList.add('sticky');
+    document.querySelector('#menu').classList.remove('visable');
   } else {
+    document.querySelector('#menu').classList.add('visable');
     navBar.classList.remove('sticky');
   }
-
+  let tmp = {};
   navBarLinks.forEach((link) => {
     let section = document.querySelector(link.hash);
+    let calc = section.offsetTop + section.clientHeight - scrollPos;
+    if (calc <= 0) calc = 99999999999999;
+    tmp[section.id] = calc;
     if (
       scrollPos > section.offsetTop &&
       scrollPos < section.offsetTop + section.clientHeight
     ) {
-      link.parentElement.classList.add('inactive');
+      link.parentElement.classList.add('active');
     } else {
-      link.parentElement.classList.remove('inactive');
+      link.parentElement.classList.remove('active');
     }
   });
-  if (scrollPos === 0 || navBarLinks === '#navBar') {
-    navWil.classList.add('inactive');
-  }
+  console.log(tmp);
+  let key = Object.keys(tmp).reduce((key, v) => (tmp[v] < tmp[key] ? v : key)); // copyed from stackoverflow, get the min value in an object and return the key
+  document.querySelector('#navBar > li').classList.remove('active');
+  document
+    .querySelector(`[href="#${key}"]`)
+    .parentElement.classList.add('active');
 });
